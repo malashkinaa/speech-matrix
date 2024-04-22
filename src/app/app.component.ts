e0174c7d2203347c35fcf44dd70277c4a956bf5d
Yana Malashkina <120207504+malashkinaa@users.noreply.github.com>
2024-04-22 23:40:18 +0200

    innitial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { HeaderComponent } from './header/header.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeftPanelComponent, HeaderComponent, RightPanelComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'speech-matrix';
+ 
 }
